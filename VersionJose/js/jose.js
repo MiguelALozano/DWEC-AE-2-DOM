1bc2c0f9b8842c0f898e3b89b95dd21d5302aeae
JoseASantoss <j.santos.sanchez@gmail.com>
2021-10-22 13:58:08 +0200

    Formulario con literales

--- VersionJose/js/jose.js
+++ VersionJose/js/jose.js
@@ -2,38 +2,82 @@ function cargarFormulario() {
 
     let texto = document.getElementById("formulario");
 
-    texto.innerHTML= `<form action="" method="get">
-    <label for="dni"></label>
-    <input type="text" name="dni" id="dni" placeholder="DNI"><br>
-    <label for="nombre"></label>
-    <input type="text" name="nombre" id="nombre" placeholder="Nombre"><br>
-    <label for="apellidos"></label>
-    <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos"><br>
-    <label for="telefono"></label>
-    <input type="tel" name="telefono" id="telefono" placeholder="Telefono"><br>
-    <label>Genero:</label> 
-    <input type="radio" name="hombre" id="hombre" >
-    <label> Hombre</label>
-    <input type="radio" name="mujer" id="mujer" >
-    <label> Mujer</label>
-    <input type="radio" name="indefinido" id="indefinido" checked >
-    <label> Indefinido</label>
-    <div id="imagenes">
-        <img src="#" alt="aquí imagen1">
-        <img src="#" alt="aquí imagen2">
+    texto.innerHTML= `<form id="formulario" action="" method="get">
+    <h2>Academia de baile - "Los 4 Jinetes"</h2>
+    <div class="campos">
+        <div>
+            <label for="dni"></label>
+            <input type="text" name="dni" id="dni" placeholder="DNI">
+        </div>
+        <div>
+            <label for="nombre"></label>
+            <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+        </div>
+        <div>
+            <label for="apellidos"></label>
+            <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos">
+        </div>
+        <div>
+            <label for="apellidos"></label>
+            <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos">
+        </div>
+        <div>
+            <label for="telefono"></label>
+            <input type="tel" name="telefono" id="telefono" placeholder="Telefono">
+        </div>
+        <div>
+            <label>Genero:</label> 
+            <input type="radio" name="hombre" id="hombre" >
+            <label> Hombre</label>
+            <input type="radio" name="mujer" id="mujer" >
+            <label> Mujer</label>
+            <input type="radio" name="indefinido" id="indefinido" checked >
+            <label> Indefinido</label>
+        </div>
+        <div>
+            <p>Qué te gusta bailar</p>
+            <label>Salsa </label>
+            <input type="checkbox" name="salsa" id="salsa">
+            <label>Bachata </label>
+            <input type="checkbox" name="bachata" id="bachata">
+            <label>Kizomba</label>
+            <input type="checkbox" name="kizomba" id="kizomba">
+            <label>Salón </label>
+            <input type="checkbox" name="salon" id="salon">
+            <label>Yo soy de barra</label>
+            <input type="checkbox" name="barra" id="barra">
+        </div>
     </div>
-    <label for="gustos">Que color te gusta más: </label>
-    <select name="gustos" id="gustos">
-        <option value="verde">Verde</option>
-        <option value="azul">Azul</option>
-        <option value="rojo">Rojo</option>
-        <option value="verde">Verde</option>
-    </select><br>
-    <label for="texto">Dinos que piensas: </label><br>
-    <textarea name="texto" id="texto" cols="50" rows="10" placeholder="Aquí puedes soltarlo todo...."></textarea>
-    <br>
-    <input type="reset" value="Borrar">
-    <input type="submit" value="Enviar">
+    <div id="imagenes">
+        <div id="foto1">
+            <img src="/VersionJose/images/preillumination-seth-M12-_qST8iQ-unsplash.jpg" alt="Bailes de salon">
+            <div>Photo by <a href="https://unsplash.com/@7seth?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Preillumination SeTh</a> on <a href="https://unsplash.com/s/photos/dance?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+        </div>
+        <div id="foto2">
+            <img src="/VersionJose/images/ardian-lumi-6Woj_wozqmA-unsplash.jpg" alt="sbk">
+            <div>Photo by <a href="https://unsplash.com/@ardianlumi?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ardian Lumi</a> on <a href="https://unsplash.com/s/photos/ballrom-dance?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+            </div>
+        </div>
+    </div>
+    <div class="select">
+        <label for="gustos">Que estilo te gusta mas?: </label>
+        <select name="gustos" id="gustos">
+            <option value="salsa">Salsa</option>
+            <option value="bachata">Bachata</option>
+            <option value="kizomba">Kizomba</option>
+            <option value="valsIngles">Vals Ingles</option>
+            <option value="tango">Tango</option>
+            <option value="tango">Otros</option>
+        </select>
+    </div>
+   <div class="textarea">
+        <textarea name="texto" id="texto" placeholder="Cuentanos que otros estilos de baile te gustan..."></textarea>
+   </div>
+   <div class="botones">
+        <input type="reset" value="Borrar">
+        <input type="submit" value="Enviar">
+   </div>
 </form>`
     
 
